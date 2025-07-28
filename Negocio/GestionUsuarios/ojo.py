e54671cad23a701cc69f558663ee084873d5e0fc
Brandolh31 <lbrandolh@gmail.com>
2025-07-28 12:31:21 -0400

    creacion clase usuario

---
+++ Negocio/GestionUsuarios/ojo.py
@@ -0,0 +1,23 @@
+class Usuario:
+    def __init__(self, nombre, telefono, correo):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.correo = correo
+
+    def getNombre(self):
+        return self.nombre
+    
+    def getTelefono(self):
+        return self.telefono
+    
+    def getCorreo(self):
+        return self.correo
+
+    def setNombre(self, nombre):
+        self.nombre = nombre
+    
+    def setTelefono(self, telefono):
+        self.telefono = telefono
+    
+    def setCorreo(self, correo):
+        self.correo = correo
